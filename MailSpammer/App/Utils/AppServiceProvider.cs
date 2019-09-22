f6f69ace832fef4aa4f6c62fe71b969bc9d66b30
Chlipp Szymon <chlippszymon@gmail.com>
2019-09-22 19:46:39 +0200

    service provider implementation

---
+++ MailSpammer/App/Utils/AppServiceProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Utils
+{
+    public static class AppServiceProvider
+    {
+        public static void Init(ServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        private static ServiceProvider _provider;
+
+        public static TService Get<TService>()
+        {
+            return _provider.GetService<TService>();
+        }
+    }
+}
