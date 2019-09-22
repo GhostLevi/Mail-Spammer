5fa8f737ca97d3f0255934ae06192e9f2e83be4f
Chlipp Szymon <chlippszymon@gmail.com>
2019-09-22 19:46:52 +0200

    app logger implementation

---
+++ MailSpammer/App/Utils/AppLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+
+namespace App.Utils
+{
+    public static class AppLogger
+    {
+        public static void Information(string info)
+        {
+            Log.Information($":: MAIL SPAMMER INFORMATION :: {info}");
+        }
+
+        public static void ErrorEx(Exception ex)
+        {
+            Log.Error($":: MAIL SPAMMER ERROR :: An {ex.Message} has been thrown ");
+        }
+
+        public static void Error(string info)
+        {
+            Log.Error($":: MAIL SPAMMER ERROR :: {info}");
+        }
+    }
+}
